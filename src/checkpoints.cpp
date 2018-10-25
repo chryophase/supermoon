b449563cf8ae6df0bccd9d0b04a742a76aa6e67c
David Chapman <crypto@supermooncoin.com>
2018-10-26 06:00:17 +1000

    Checkpoint Update for 1.2.0.1
    Checkpoint Update for 1.2.0.1

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -3,7 +3,7 @@
 // Copyright (c) 2018 Supermoon Developers Prod Checkpoint 1.0753
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-// Supermoon version v1.21  PRODUCTION RELEASE  1.2.0.1 	(Oct 13 2018) - POS 3.5
+// Supermoon version v1.21  PRODUCTION RELEASE  1.2.0.1 	(Oct 26 2018) - POS 3.5
 
 #include <boost/assign/list_of.hpp> // for 'map_list_of()'
 #include <boost/foreach.hpp>
@@ -26,6 +26,7 @@ namespace Checkpoints
         (      0,    	uint256("0x00000370a125304fe9ee469e9f0b3197dbff231296a1be06f7c6f3104e3b6ad8") )
         (   4000,       uint256("0x1c9b64c14f5268569ed2d1fd56bec7c4627513f60422bd1a4f2469382e194744") )
         (   7000,       uint256("0x10f5dbccc688cdc9a3ff1a4dfd9901a2f2de8a10725ad17257316d55f76fe2e5") )
+        (  10000,       uint256("0x0935005400c7e550211817cd82d88da71c62e5d876f955ebc7464754e6ed8ae6") )
         ;
 //	(   6000,    uint256("0x") )
 //	(   11000,   uint256("0x") )
